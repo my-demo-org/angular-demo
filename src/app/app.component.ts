805ec8e03146bd777de8c9b4a6679102974b4b98
mengshuicmq <alive_cmq@163.com>
2019-01-10 11:24:01 +0800

    animation css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-angular-demo';
+export class AppComponent implements OnInit {
+  disabled = false;
+
+  showBlock = false;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  start() {
+    this.showBlock = true;
+    this.disabled = true;
+  }
+
+  reset() {
+    this.showBlock = false;
+    this.disabled = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AnimationsComponent } from './animations/animations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimationsComponent
   ],
   imports: [
     BrowserModule,
